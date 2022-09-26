1bcecf3f084e592fdc989a46d670bdcde0fd4c78
rainmanxn <47183790+rainmanxn@users.noreply.github.com>
2022-09-26 10:34:17 +0300

    add not found

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -8,9 +8,16 @@ describe('classNames', () => {
         expect(classNames('testCl', {}, ['cl1', 'cl2'])).toBe('testCl cl1 cl2');
     });
     test('all params 1 false', () => {
-        expect(classNames('testCl', { hovered: true, scrolled: false }, ['cl1', 'cl2'])).toBe('testCl cl1 cl2 hovered');
+        expect(
+            classNames(
+                'testCl',
+                { hovered: true, scrolled: false },
+                ['cl1', 'cl2'],
+            ),
+        )
+            .toBe('testCl cl1 cl2 hovered');
     });
     test('all params 1 undefined', () => {
-      expect(classNames('testCl', { hovered: true, scrolled: undefined }, ['cl1', 'cl2'])).toBe('testCl cl1 cl2 hovered');
+        expect(classNames('testCl', { hovered: true, scrolled: undefined }, ['cl1', 'cl2'])).toBe('testCl cl1 cl2 hovered');
     });
 });
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -22,7 +24,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 onClick={onToggle}
                 type="button"
             >
-                Toggle
+                {t('Переключить')}
             </button>
             <div className={styles.switchers}>
                 <ThemeSwitcher />
