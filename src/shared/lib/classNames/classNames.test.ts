d5ffccd2e35d0a787bb71b137b60d0b274d76666
rainmanxn <47183790+rainmanxn@users.noreply.github.com>
2022-09-26 08:08:31 +0300

    add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,16 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('First param only', () => {
+        expect(classNames('testCl')).toBe('testCl');
+    });
+    test('2 params', () => {
+        expect(classNames('testCl', {}, ['cl1', 'cl2'])).toBe('testCl cl1 cl2');
+    });
+    test('all params 1 false', () => {
+        expect(classNames('testCl', { hovered: true, scrolled: false }, ['cl1', 'cl2'])).toBe('testCl cl1 cl2 hovered');
+    });
+    test('all params 1 undefined', () => {
+      expect(classNames('testCl', { hovered: true, scrolled: undefined }, ['cl1', 'cl2'])).toBe('testCl cl1 cl2 hovered');
+    });
+});
